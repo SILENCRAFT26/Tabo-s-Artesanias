c7a580a44426302eaf2e84fdd1bdae0a33eb4d79
Gustavo Castrillo <59183567+SILENCRAFT26@users.noreply.github.com>
2022-05-14 19:27:01 -0600

    Venta

---
+++ js/masVentas.js
@@ -0,0 +1,12 @@
+
+var btnDer, btnIzq;
+
+btnIzq = document.getElementsByClassName('btn-izq-num')
+btnDer = document.getElementsByClassName('btn-der-num')
+btnDer.addEventListener('click',()=>{
+
+});
+
+btnIzq.addEventListener('click',()=>{
+
+});
